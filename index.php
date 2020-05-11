f147965743c84285291bf468ffdba454a20fb3cf
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-11 10:46:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Dashboard - RSKIA Rachmi Yogyakarta</title>
+    <title>SIMETRIS - RSKIA Rachmi Yogyakarta</title>
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
