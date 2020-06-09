57dac27fb118bff4861ec6c7d718e75446a7705a
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-09 16:18:31 +0700

    Add files via upload

--- notifikasi1.php
+++ notifikasi1.php
@@ -1,4 +1,4 @@
 <div class="alert alert-info alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    Selamat Datang di Sistem Informasi RSKIA Rachmi Yogyakarta
+    <font size='3'>Selamat Datang di Sistem Informasi Rumah Sakit dikembangkan oleh Tim IT RSKIA Rachmi Yogyakarta</font>
 </div>
---
+++ tes.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dashboard - RSKIA Rachmi Yogyakarta</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <!-- Add custom CSS here -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <!-- Page Specific CSS -->
+    <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
+    <script type="text/javascript" src="chartjs/Chart.js"></script>
+  </head>
+  <body>
+  <nav>
+    <div id="wrapper">
+        </div><!-- /.navbar-collapse -->
+      </nav>
+      <div id="page-wrapper">
+      	<img class="img-responsive" src="images/header.jpg" width="100%" alt="Gambar Bootstrap 3"><br>
+        <div class="row">
+          <div class="col-lg-12">
+            <?php include "notifikasi1.php"?>
+          </div>
+        </div><!-- /.row -->
+  <div class="row">
+    <div class="col-lg-12">
+<table border="1">
+<thead>
+<tr>
+<th rowspan="4"><center>NO</center></th>
+<th rowspan="4"><center>TINDAKAN</center></th>
+<th colspan="2"><center>VIP</center></th>
+<th><center>KELAS 1</center></th>
+<th><center>KELAS 2</center></th>
+<th><center>KELAS 3</center></th>
+</tr>
+<tr>
+<th><center>Anturium</center></th>
+<th><center>Sakura</center></th>
+<th><center>Cempaka</center></th>
+<th rowspan="3"><center>Mawar</center></th>
+<th rowspan="3"><center>Flamboyan</center></th>
+</tr>
+<tr>
+<th><center>Anggrek</center></th>
+<th rowspan="2"><center>Lavender</center></th>
+<th rowspan="2"><center>Edelweis</center></th>
+</tr>
+<tr>
+<th><center>Bougenville</center></th>
+</tr>
+<tr>
+<td><center>1</center></th>
+<td><left>Persalinan Normal 3 Hari</center></th>
+<td><center>Rp 7.950.000</center></td>
+<td><center>Rp 7.150.000</center></td>
+<td><center>Rp 6.200.000</center></td>
+<td><center>Rp 5.450.000</center></td>
+<td><center>Rp 4.950.000</center></td>
+</tr>
+<tr>
+<td><center>2</center></th>
+<td><left>Persalinan dengan Penyulit 3 Hari</center></th>
+<td><center>Rp 8.650.000</center></td>
+<td><center>Rp 7.800.000</center></td>
+<td><center>Rp 6.800.000</center></td>
+<td><center>Rp 6.000.000</center></td>
+<td><center>Rp 5.500.000</center></td>
+</tr>
+<tr>
+<td><center>3</center></th>
+<td><left>Operasi Caesar 4 Hari</center></th>
+<td><center>Rp 14.650.000</center></td>
+<td><center>Rp 13.500.000</center></td>
+<td><center>Rp 11.700.000</center></td>
+<td><center>Rp 10.600.000</center></td>
+<td><center>Rp 9.700.000</center></td>
+</tr>
+<tr>
+<td><center>4</center></th>
+<td><left>Operasi Caesar dengan Penyulit 4 Hari</center></th>
+<td><center>Rp 15.700.000</center></td>
+<td><center>Rp 14.400.000</center></td>
+<td><center>Rp 12.600.000</center></td>
+<td><center>Rp 11.400.000</center></td>
+<td><center>Rp 10.400.000</center></td>
+</tr>
+<tr>
+<td><center>5</center></th>
+<td><left>Operasi Minor (Curetase, dsi) 1 Hari</center></th>
+<td><center>Rp 5.300.000</center></td>
+<td><center>Rp 4.700.000</center></td>
+<td><center>Rp 4.150.000</center></td>
+<td><center>Rp 3.600.000</center></td>
+<td><center>Rp 3.200.000</center></td>
+</tr>
+<tr>
+<td><center>6</center></th>
+<td><left>Operasi Sedang (MOW, Sirkumsisi, dsi) 1 Hari</center></th>
+<td><center>Rp 6.950.000</center></td>
+<td><center>Rp 6.550.000</center></td>
+<td><center>Rp 5.700.000</center></td>
+<td><center>Rp 5.150.000</center></td>
+<td><center>Rp 4.800.000</center></td>
+</tr>
+<tr>
+<td><center>7</center></th>
+<td><left>Operasi Mayor non Caesar 4 Hari</center></th>
+<td><center>Rp 13.000.000</center></td>
+<td><center>Rp 11.950.000</center></td>
+<td><center>Rp 10.100.000</center></td>
+<td><center>Rp 9.200.000</center></td>
+<td><center>Rp 8.400.000</center></td>
+</tr>
+<tr>
+<td><center>8</center></th>
+<td><left>Perawatan Istirahat 2 Hari</center></th>
+<td><center>Rp 2.550.000</center></td>
+<td><center>Rp 2.400.000</center></td>
+<td><center>Rp 2.050.000</center></td>
+<td><center>Rp 1.750.000</center></td>
+<td><center>Rp 1.600.000</center></td>
+</tr>
+<tr>
+<td><center>9</center></th>
+<td><left>Rawat Inap Anak 3 Hari</center></th>
+<td><center>Rp 3.500.000</center></td>
+<td><center>Rp 3.300.000</center></td>
+<td><center>Rp 2.750.000</center></td>
+<td><center>Rp 2.350.000</center></td>
+<td><center>Rp 2.100.000</center></td>
+</tr>
+</thead>
+<tbody>
+
+</tbody>
+</table>
+&nbsp;
+<div align="right">Diperbarui Mei 2020</div>
+    </div><br><br><br><?php include "copyright.php";?>
+  </div><!-- /.row -->
+<br><br>
+      </div><!-- /#page-wrapper -->
+    </div><!-- /#wrapper -->
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- Page Specific Plugins -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+    <script src="js/morris/chart-data-morris.js"></script>
+    <script src="js/tablesorter/jquery.tablesorter.js"></script>
+    <script src="js/tablesorter/tables.js"></script>
+
+  </body>
+</html>
