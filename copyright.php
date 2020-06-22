58ab5f1435f8d513cbf89ce59bc14fb12363bbb9
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-22 17:01:36 +0700

    Add files via upload

--- copyright.php
+++ copyright.php
@@ -1,2 +1,12 @@
-<div align="center"><b>Developed by Ardi Tri Heru</b><br>Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script><a expr:href='data:blog.homepageUrl'><data:blog.title/>.</a> All rights reserved.<br><a href="https://instagram.com/arditriheru"><b class="bluetext">Follow Me on Instagram</b></a><br><font face="consolas" >Version 5.0.Beta</font></div>
+<!--
+	Name        : SIMETRIS
+	Description : Sistem Informasi Rumah Sakit.
+	Date        : 2019
+	Developer   : Ardi Tri Heru
+	Phone/WA    : 0896-2967-1717
+	Email       : arditriheruh@gmail.com
+	Website     : https://www.arditriheru.com
+-->
+<div align="center"><b>Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script></b> All Right Reserved.  
+<br><a expr:href='data:blog.homepageUrl'><data:blog.title/></a> <a href="https://instagram.com/arditriheru"><b class="bluetext">Tim Teknologi Informasi</b></a><br>Long Term Support<br><font face="consolas" >Versi 5.0</font></div>
 <br><br>
